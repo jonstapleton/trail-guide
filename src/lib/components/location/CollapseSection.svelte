89439b1b74ab8926b37eedf4219e60c029815ee1
Jon Stapleton <stapleton.jon.michael@gmail.com>
2024-02-22 10:34:54 -0500

    added support for collapse & aside directives

---
+++ src/lib/components/location/CollapseSection.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    export let title = "Click Me!"
+    let collapse = false;
+</script>
+
+<svelte:options customElement={{
+    tag: 'collapse-section'
+}} />
+
+<div class='collapse'>
+    <div class='has-text-centered'>
+        <button on:click={() => collapse = !collapse} class='button is-fullwidth is-dark'>{title}</button>
+    </div>
+    {#if collapse}
+    <div class='collapsible'>
+        <slot />
+    </div>
+    {/if}
+</div>
+
+<style lang='scss'>
+    @import "../../../app.scss";
+</style>
--- src/lib/components/location/directives.ts
+++ src/lib/components/location/directives.ts
@@ -1,4 +1,6 @@
 import PracticeQuestion from "./PracticeQuestion.svelte";
 import CodeAndImage from "./CodeAndImage.svelte";
+import Aside from "./Aside.svelte";
+import CollapseSection from "./CollapseSection.svelte";
 
-export { PracticeQuestion, CodeAndImage }
+export { PracticeQuestion, CodeAndImage, Aside, CollapseSection }
