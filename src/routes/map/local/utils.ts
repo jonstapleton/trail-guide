e6726cde5fbb02ae57cc9e780de2f2270d85f11b
Jon Stapleton <stapleton.jon.michael@gmail.com>
2024-07-21 20:48:08 -0400

    added lerp back

--- src/routes/map/local/utils.ts
+++ src/routes/map/local/utils.ts
@@ -271,6 +271,7 @@ export class Camera {
     lx:number = 0; ly:number = 0;
     ix:number; iy:number
     transform:Transform = { x: 0, y: 0, scale: 1}
+    lerpTransform:Transform = { x: 0, y: 0, scale: 1}
     matrix:DOMMatrix;
     offsetX:number = 0; offsetY:number = 0
     moving:boolean = false
@@ -296,19 +297,24 @@ export class Camera {
     }
 
     display(cb:any) {
+        this.lerpTransform = {
+            x: this.lerpTransform.x + ((this.transform.x - this.lerpTransform.x)/4),
+            y: this.lerpTransform.y + ((this.transform.y - this.lerpTransform.y)/4),
+            scale: this.lerpTransform.scale + ((this.transform.scale - this.lerpTransform.scale)/4)
+        }
         this.p5.push()
-        this.p5.translate(-this.transform.x, -this.transform.y)
-        this.p5.scale(this.transform.scale)
+        this.p5.translate(-this.lerpTransform.x, -this.lerpTransform.y)
+        this.p5.scale(this.lerpTransform.scale)
         cb();
         this.matrix = this.p5.drawingContext.getTransform()
         this.p5.pop()
 
-        this.p5.fill('rgba(0, 0, 0, 0')
-        this.p5.circle(this.p5.width/2, this.p5.height/2, 10)
-        this.p5.line(this.p5.width/2, this.p5.height/2 - 15, this.p5.width/2, this.p5.height/2 + 15)
-        this.p5.line(this.p5.width/2 + 15, this.p5.height/2, this.p5.width/2 - 15, this.p5.height/2)
-        this.p5.fill(0)
-        this.p5.text(`${this.x},${this.y}`, this.p5.width/2, this.p5.height/2 - 30)
+        // this.p5.fill('rgba(0, 0, 0, 0')
+        // this.p5.circle(this.p5.width/2, this.p5.height/2, 10)
+        // this.p5.line(this.p5.width/2, this.p5.height/2 - 15, this.p5.width/2, this.p5.height/2 + 15)
+        // this.p5.line(this.p5.width/2 + 15, this.p5.height/2, this.p5.width/2 - 15, this.p5.height/2)
+        // this.p5.fill(0)
+        // this.p5.text(`${this.x},${this.y}`, this.p5.width/2, this.p5.height/2 - 30)
     }
     zoom(absoluteLocation:Coords, deltaZoom:number, absolute:boolean) {
         let factor;
